595001a9d6400c79b4341affd9d3919cb9132c72
Sean Woleslagle <jacx92@gmail.com>
2020-02-01 19:06:39 -0500

    Made changes to JS and CSS

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const grid = document.querySelector("#grid-container");
 
 
-// Create default grid of 16x16
-createGrid(16);
 
 function createGrid(size) {
     for (i = 0; i < size**2; i++) { // Starting at a size of 0, increase the size of the x and y axis by size squared
         let cell = document.createElement("div");
-        cell++;
+        cell.classList.add("cell");
+        grid.appendChild(cell);;
     }
 }
+
+// Create default grid of 16x16
+createGrid(16);
