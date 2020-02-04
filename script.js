1fa1079d63084226bd8536420f41fe20916850ef
Sean Woleslagle <jacx92@gmail.com>
2020-02-04 02:41:21 -0500

    Made changes to JS file

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const grid = document.querySelector("#grid-container");
+const container = document.querySelector('.container');
+const cell = document.querySelectorAll('.square-div');
 
 
+<<<<<<< HEAD
 
 function createGrid(size) {
     for (i = 0; i < size**2; i++) { // Starting at a size of 0, increase the size of the x and y axis by size squared
@@ -10,5 +12,24 @@ function createGrid(size) {
     }
 }
 
+=======
+function createGrid(size) {
+    for (i = 0; i < size**2; i++) { // Starting at a size of 0, increase the size of the x and y axis by size squared
+        let div = document.createElement('div');
+        div.classList.toggle('square-div');
+        container.appendChild(div);
+    }
+}
+
+function hover() {
+    cell.forEach(cell => {cell.addEventListener('mouseover', console.log('hello!'))});
+}
+
+console.log(hover());
+
+
+
+
+>>>>>>> Made changes to JS
 // Create default grid of 16x16
 createGrid(16);
